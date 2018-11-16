e777003885e9568ad0d4f72096039e2370a5eb8a
Noor Ncho <nrandin@gmail.com>
2018-11-16 17:11:00 -0500

    Delete Constants.java

--- src/Constants.java
+++
@@ -1,9 +0,0 @@
-
-public class Constants 
-{ //General starter constants that are needed in the game 
-	public static int SUNFLOWER_HEALTH = 50;
-	public static int ZOMBIE_HEALTH = 100;
-	public static int PEA_SHOOTER_HEALTH = 100; 
-	public static int PEA_SHOOTER_COST = 100;
-	public static int SUNFLOWER_COST = 50;
-}
