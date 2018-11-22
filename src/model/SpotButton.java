34ff3a8c011be9543241839f0daeb666e96f6873
Robyn <robynvg21@gmail.com>
2018-11-21 20:47:54 -0500

    fixed image path problem

--- src/model/SpotButton.java
+++ src/model/SpotButton.java
@@ -23,30 +23,43 @@ public class SpotButton extends JButton
 		return spotButtonID;
 	}
 
+	//// contained in the classpath
+	//URL url = getClass().getResource("/path/to/image.jpg");
+	//Image image = new ImageIcon(url).getImage();
 	public void addZombie() 
 	{
-		ImageIcon zombieIcon = new ImageIcon(getClass().getResource("/resources/zombie.png"));
-		Image zombieImage = zombieIcon.getImage();
-	    Image newZombieImage = zombieImage.getScaledInstance(25, 25, Image.SCALE_SMOOTH);
-	    zombieIcon = new ImageIcon(newZombieImage);
-	    setIcon(zombieIcon);
+		try {
+			ImageIcon zombieIcon = new ImageIcon("resources/zombie.png");
+			Image zombieImage = zombieIcon.getImage().getScaledInstance(25, 25, Image.SCALE_SMOOTH);
+			setIcon(new ImageIcon(zombieImage));
+			setDisabledIcon(new ImageIcon(zombieImage));
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
 	}
 	
 	public void addSunflower() 
 	{
-		 ImageIcon sunflowerIcon = new ImageIcon("C:\\Users\\dennis\\eclipse-workspace\\PVZVIEW\\src\\sunflower.png");
-		 Image sunflowerImage = sunflowerIcon.getImage();
-		 Image newSunflowerImage = sunflowerImage.getScaledInstance(25, 25, Image.SCALE_SMOOTH);
-		 sunflowerIcon = new ImageIcon(newSunflowerImage);
-		 setIcon(sunflowerIcon);
+		try {
+			ImageIcon sunflowerIcon = new ImageIcon("resources/sunflower.png");
+			Image sunflowerImage = sunflowerIcon.getImage().getScaledInstance(25, 25, Image.SCALE_SMOOTH);
+			setIcon(new ImageIcon(sunflowerImage));
+			setDisabledIcon(new ImageIcon(sunflowerImage));
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
 	}
 	
 	public void addPeashooter() 
 	{
-		ImageIcon peaShooterIcon = new ImageIcon("C:\\Users\\dennis\\eclipse-workspace\\PVZVIEW\\src\\peashooter.png");
-	    Image peaShooterImage = peaShooterIcon.getImage();
-	    Image newPeaShooterImage = peaShooterImage.getScaledInstance(25, 25, Image.SCALE_SMOOTH);
-	    peaShooterIcon = new ImageIcon(newPeaShooterImage);
-	    setIcon(peaShooterIcon);
+		try {
+			ImageIcon peashooterIcon = new ImageIcon("resources/peashooter.png");
+			Image peashooterImage = peashooterIcon.getImage().getScaledInstance(25, 25, Image.SCALE_SMOOTH);
+			setIcon(new ImageIcon(peashooterImage));
+			//Image won't lose colour when button is disabled
+			setDisabledIcon(new ImageIcon(peashooterImage));
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
 	}	
 }
