31801fc2d71e56b6f225797d6e89ad68a9c5ac48
PRAGYA SINGH <32852586+PragyaJollySingh@users.noreply.github.com>
2018-11-25 19:27:56 -0500

    Update Plant.java
    comments

--- src/model/Plant.java
+++ src/model/Plant.java
@@ -6,7 +6,7 @@ public class Plant
 	private int cost; 
 	private boolean alive;
 	
-	
+	// This is the general plant class as no matter what kind of plant you always have to keep track of the health and cost
 	public Plant(int health, int cost) 
 	{
 		this.health = health;
