640e4e9f4fd4e5e854b5da6283c46095ab94e0f9
PRAGYA SINGH <32852586+PragyaJollySingh@users.noreply.github.com>
2018-11-25 19:15:40 -0500

    Update GridButtonPanel.java
    comments

--- src/view/GridButtonPanel.java
+++ src/view/GridButtonPanel.java
@@ -26,7 +26,7 @@ public class GridButtonPanel extends JPanel
 	private SpotButton C1, C2, C3,C4,C5,C6,C7,C8,C9,C10;
 	private SpotButton D1,D2,D3,D4,D5,D6,D7,D8,D9,D10;
 	private SpotButton E1,E2,E3,E4,E5,E6,E7,E8,E9,E10;
-	private ArrayList<SpotButton>gridButtons;
+	private ArrayList<SpotButton>gridButtons; // holds the array of all the buttons
 	private Controller controller;
 	
 	public GridButtonPanel() 
@@ -56,7 +56,7 @@ public class GridButtonPanel extends JPanel
 		}
 	}
 	
-	public void enableGridButtons() 
+	public void enableGridButtons() // disables the button so that the player can not add ample number of plants in one grid button.
 	{
 		for(SpotButton sb : gridButtons) 
 		{
